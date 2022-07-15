d5473ee86b776bb1e6b03e06f8bd9ea35c4864a8
Philipp Scheer <scheer28philipp@gmail.com>
2022-07-15 15:23:19 +0200

    add video and image endpoints

--- rockeet.js
+++ rockeet.js
@@ -7,7 +7,7 @@ module.exports = {
     Assistant: require("./lib/assistant"),
     File: require("./lib/file"),
     Image: require("./lib/image"),
-    // Video: require("./lib/video"),
+    Video: require("./lib/video"),
     Audio: require("./lib/audio"),
 
     setToken: require("./lib/_helper").setToken,
